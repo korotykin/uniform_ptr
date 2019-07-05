ceebe55aaabc205e21168cf5f0d6f09b9366577b
Korotykin <alexander.korotykin@gmail.com>
2019-07-05 22:22:38 +0400

    init

--- AbstractStorageTest/AbstractStorageTest.cpp
+++ AbstractStorageTest/AbstractStorageTest.cpp
@@ -1,21 +1,22 @@
-// AbstractStorageTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
 #include <iostream>
+
+class StoredInt {
+public:
+	StoredInt(const int val): mValue(val)
+	{}
+public:
+	int AddVal(const int val)
+	{
+		return mValue + val;
+	}
+private:
+	int mValue = 0;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	StoredInt v(3);
+    std::cout << v.AddVal(2) << std::endl;
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
