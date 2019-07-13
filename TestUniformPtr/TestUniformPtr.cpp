a280ef1d628fd781963eee0f1bc5c7eb1ea2f122
Korotykin <alexander.korotykin@gmail.com>
2019-07-13 23:58:19 +0400

    added test for nonCopyable object

--- TestUniformPtr/TestUniformPtr.cpp
+++ TestUniformPtr/TestUniformPtr.cpp
@@ -49,11 +49,13 @@ BOOST_AUTO_TEST_CASE(test_uniform_ptr)
 	BOOST_CHECK(1 == *uniform_ptr<int>{1}); // value is moved
 	int b = 2;
 	BOOST_CHECK(2 == *uniform_ptr<int>{b}); // value is copyed
-	BOOST_CHECK(2 == *uniform_ptr<int>{&b}); // saving a pointer
+	BOOST_CHECK(2 == *uniform_ptr<int>{&b}); // saving a pointer to value
 
+	// checking with objects
 	static_assert(!std::is_move_constructible_v<IntNonMovable>);
-	BOOST_CHECK(2 == uniform_ptr<IntNonMovable>(IntNonMovable(2))->get()); // copying value
+	BOOST_CHECK(4 == uniform_ptr<IntNonMovable>(IntNonMovable(4))->get()); // copying value
+	BOOST_CHECK(6 == uniform_ptr<IntValue>{IntNonMovable{ 6 }}->get());
 	static_assert(!std::is_copy_constructible_v<IntNonCopyable>);
 	BOOST_CHECK(3 == uniform_ptr<IntNonCopyable>(IntNonCopyable(3))->get()); // moving value
-	BOOST_CHECK(3 == uniform_ptr<IntValue>(IntNonMovable(3))->get()); // copying child value
+	BOOST_CHECK(5 == uniform_ptr<IntValue>(IntNonCopyable(5))->get()); // copying child value
 }
